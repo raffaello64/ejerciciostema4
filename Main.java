3ba0f07fa9a66721c38dcaf3f413127076cc460a
raffaello64 <108016017+raffaello64@users.noreply.github.com>
2022-07-20 19:17:28 +0200

    Bucle do_while
    Bucle con la misma estructura While, pero solo se ejecuta una vez

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-package While;
+package do_while;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int contador = 0;
+        int contador = 3;
         
-        while (contador < 3) {
+        do {
         	System.out.println(contador);
-        	contador ++;
-        	
-        }
+        	contador = contador ++;
+        } while (contador > 3);
 	}
 
 }
