94d51aae7ae506b08d8bd95f723683932e8661da
raffaello64 <108016017+raffaello64@users.noreply.github.com>
2022-07-20 19:02:04 +0200

    Ejercicio bucle While
    Ejercicio con variable "while" en la que esta es inferior a 3 y el valor se incrementa de 1 en 1 mientras se muestra en pantalla

--- Main.java
+++ Main.java
@@ -1,26 +1,16 @@
-package Positivo_negativo;
-
-import java.util.Scanner;
+package While;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Introduzca un numero: ");
+        int contador = 0;
         
-        int numero = sc.nextInt();
-        
-		if (numero == 0) {
-        	System.out.println("El numero es 0");
-        } else if (numero < 0) {
-        	System.out.println("El numero es negativo");
-        } else {
-        	System.out.println("El numero es positivo");
+        while (contador < 3) {
+        	System.out.println(contador);
+        	contador ++;
+        	
         }
-        
-      
 	}
 
 }
