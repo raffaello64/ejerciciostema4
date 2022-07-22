fca5f395d4d9bb82080a7ac57b1c9bf0c28d44f1
raffaello64 <108016017+raffaello64@users.noreply.github.com>
2022-07-22 14:09:20 +0200

    Ejercicio Switch
    Ejercicio Switch con variable "estacion" y distintos case. Dependiendo de la esstacion ingresada, se presenta un mensaje con respecto a esta.

--- Main.java
+++ Main.java
@@ -1,12 +1,34 @@
-package For;
+package Switch;
+
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1) {
-        	System.out.println(numeroFor);
-        } 
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese una estacion: ");
+        String estacion = sc.nextLine();
+        switch (estacion) {
+        
+        case "primavera":
+        	System.out.println("Es primavera, cuidate del polen");
+        	break;
+        case "verano":
+        	System.out.println("Es verano, hay que ir a la playa");
+        	break;
+        case "otono":
+        	System.out.println("Es otono, puedes ir a buscar setas");
+        	break;
+        case "invierno":
+        	System.out.println("Es invierno, hay que abrigarse");
+        	break;
+        default:
+        	System.out.println("No ha ingresado una estacion, por favor, ingrese primavera, verano, otono o invierno");
+        	break;
+        
+        
+        }
 	}
 
 }
